009f21dc05839191772efa0dcde6d7c7589e5e94
David Cortés <david.cortes.18@gmail.com>
2011-11-22 13:39:29 +0100

    GRAVATAR!!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111122121016) do
+ActiveRecord::Schema.define(:version => 20111122123133) do
 
   create_table "recipes", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20111122121016) do
     t.date     "death"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "mail"
   end
 
 end
